71a214831366544fda3ad8fc2a86dea5b717ea12
Dan-Adrian Patiu <dan.patiu@office.pitechnologies.ro>
2018-10-09 18:16:35 +0300

    fix(core): resource path issues

--- projects/core/src/lib/services/url-scope-trimmer.ts
+++ projects/core/src/lib/services/url-scope-trimmer.ts
@@ -16,6 +16,24 @@ export class UrlScopeTrimmer {
     return href.substr(matchScope.baseHref.length);
   }
 
+  unTrimToLocalScope(href: string): any {
+    const localScope = this.scopes.find(({name}) => name === 'local');
+
+    if (localScope === undefined) {
+      return href;
+    }
+
+    if (localScope.baseHref.endsWith('/') && href.startsWith('/')) {
+      localScope.baseHref.substr(0, localScope.baseHref.length - 1);
+    }
+
+    if (!localScope.baseHref.endsWith('/') && !href.startsWith('/')) {
+      return localScope.baseHref + '/' + href;
+    }
+
+    return localScope.baseHref + href;
+  }
+
   trimToScope(href: string, scope: string): any {
     const matchScope = this.scopes.find(({baseHref, scope: matchingScope}) => scope === matchingScope && href.startsWith(baseHref));
 
