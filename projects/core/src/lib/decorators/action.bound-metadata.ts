8b4fb92a2c5fb84b3af3cceb622309d923f6f862
Dan Patiu <dan.patiu@pitechnologies.ro>
2018-10-22 14:05:10 +0300

    doc(core): added documentation - action event, action listener, metadata

--- projects/core/src/lib/decorators/action.bound-metadata.ts
+++ projects/core/src/lib/decorators/action.bound-metadata.ts
@@ -1,4 +1,5 @@
 import { FieldBoundMetadata } from './meta';
+
 export class ActionBoundMetadata implements FieldBoundMetadata {
   constructor(public actionName: string, public bindingName: string) { }
 }
